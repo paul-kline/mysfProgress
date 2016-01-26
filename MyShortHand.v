044b43015f6e23fbb6039b6686abf8d6588f0f9a
Paul Kline <pauliankline@gmail.com>
2016-01-26 15:00:54 -0600

    for faster proving

---
+++ MyShortHand.v
@@ -0,0 +1,9 @@
+
+
+
+
+Tactic Notation "ass" := assumption.
+Tactic Notation "refl" := reflexivity. 
+Tactic Notation "dest" tactic (c) := destruct c .
+Tactic Notation "ind" tactic (c) := induction c. 
+Tactic Notation "inv" tactic (c) := inversion c. 
